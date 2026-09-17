c306bba1c0125f0e128bf05ad42a7eb605d0c9b0
Ronja <79447937+ronja-ui@users.noreply.github.com>
2026-09-17 08:48:15 +0300

    [Issue 503] Refactor home view (#602)
    * reorganized the front page content
    
    * reorganized the layout structure for login and front pages. elements also scale better now despite browser settings.
    
    * fixed incorrect utility class names in configurations, credentials, and jobs pages
    
    * changed background color for start page. had to make sure that the original background color stays the same in other pages by creating separate declarations.
    
    * removed new folder button since the feature is not yet supported
    
    * renamed import button to be a more clear
    
    * removed unuseful comments
    
    * removed the function related to notebook reload button
    
    * made fixes that were accidentally removed during rebasing

--- src/app/home/home.controller.ts
+++ src/app/home/home.controller.ts
@@ -62,11 +62,6 @@ function HomeCtrl($scope,
     vm.noteCustomHome = false;
   };
 
-  $scope.reloadNoteList = function() {
-    websocketMsgSrv.reloadAllNotesFromRepo();
-    $scope.isReloadingNotes = true;
-  };
-
   $scope.createNote = function(path) {
 
     noteCreateService.init(path);
