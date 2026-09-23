add1d477a607fa8de56c55e99b2b7faf180c5bfd
sryhanen <samuli.ryhanen@fail.safe.net>
2026-09-23 10:28:58 +0300

    Refactored Paragraph according to RenderNode

--- src/app/objects/paragraph/paragraphImpl.ts
+++ src/app/objects/paragraph/paragraphImpl.ts
@@ -49,10 +49,8 @@ import {OutputContainer} from '../output/container/outputContainer';
 import {OutputContainerImpl} from '../output/container/outputContainerImpl';
 import {WebSocketPayload} from '../webSocketPayload/webSocketPayload';
 import {WebSocketPayloadImpl} from '../webSocketPayload/webSocketPayloadImpl';
-import {computed, Signal} from '@angular/core';
+import {signal, Signal} from '@angular/core';
 import { RenderNode } from '../rendering/renderNode/renderNode';
-import {ComponentViewStub} from '../rendering/componentView/componentViewStub';
-import {ComponentView} from '../rendering/componentView/componentView';
 import {ResponseRegister} from '../register/responseRegister/responseRegister';
 import {
   ResponseRegisterWithPropertyFilter
@@ -67,12 +65,14 @@ import {
   RequestRegisterWithPropertyDecorator
 } from '../register/requestRegister/requestRegisterWithPropertyDecorator/requestRegisterWithPropertyDecorator';
 import {ParagraphOutputMessageFactoryImpl} from './paragraphOutputMessageFactory/paragraphOutputMessageFactoryImpl';
+import {RenderNodeImpl} from '../rendering/renderNode/renderNodeImpl';
+import {RegisteredComponents} from '../../ui/angular2+/componentRegistry/registeredComponents';
 
 export class ParagraphImpl implements Paragraph {
   private readonly _channel: Channel;
   private readonly _outputContainer: OutputContainer;
   private readonly _paragraph: WebSocketPayload;
-  private readonly _componentView: ComponentView;
+  private readonly _renderNode: Signal<RenderNode>;
   private readonly _responseRegister:ResponseRegister;
   private readonly _requestRegister:RequestRegister;
 
@@ -80,7 +80,9 @@ export class ParagraphImpl implements Paragraph {
     this._channel = channel;
     this._paragraph = new WebSocketPayloadImpl(paragraph);
     this._outputContainer = this.initializedOutputContainer(paragraph);
-    this._componentView = new ComponentViewStub();
+    this._renderNode = signal(new RenderNodeImpl(RegisteredComponents.PARAGRAPH_VIEW, signal({
+      output:this._outputContainer.print()(),
+    })));
     this._responseRegister = new ResponseRegisterWithPropertyFilter(new ResponseRegisterWithDefaultResponseList(new ResponseRegisterImpl(), [this._outputContainer]), {name:'paragraphId', type:'string'}, this.id());
     this._requestRegister = new RequestRegisterWithPropertyDecorator(new RequestRegisterImpl(this._channel), {name:'paragraphId', value: this.id()});
   }
@@ -96,10 +98,7 @@ export class ParagraphImpl implements Paragraph {
   }
 
   print(): Signal<RenderNode> {
-    return computed(() => ({
-      children:computed(() => [this._outputContainer.print()()]),
-      componentView: this._componentView
-    }));
+    return this._renderNode;
   }
 
   id(): string {
